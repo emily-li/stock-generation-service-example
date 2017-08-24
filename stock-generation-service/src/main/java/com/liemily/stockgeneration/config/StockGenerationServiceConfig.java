5bbbc7b1d1d3b7ca517cce42b39f9c72de51311a
emily-li <emily.li@live.co.uk>
2017-08-24 22:21:54 +0100

    Removing unneeded entity scan as Spring JPA is not used

--- stock-generation-service/src/main/java/com/liemily/stockgeneration/config/StockGenerationServiceConfig.java
+++ stock-generation-service/src/main/java/com/liemily/stockgeneration/config/StockGenerationServiceConfig.java
@@ -1,13 +1,12 @@
 package com.liemily.stockgeneration.config;
 
-import com.liemily.stock.config.StockConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * Created by Emily Li on 21/08/2017.
  */
 @Configuration
-@Import(StockConfig.class)
+@Lazy
 public class StockGenerationServiceConfig {
 }
